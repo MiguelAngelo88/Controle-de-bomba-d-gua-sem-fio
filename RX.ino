d6a80492e147379e1c85929f204ad644bb9059f7
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-12-19 15:17:06 -0300

    Update RX.ino

--- RX.ino
+++ RX.ino
@@ -1,124 +1,137 @@
+/*
+  Controle de Bomba d'Água via LoRa
+  Módulo RX - Receptor
+  Hardware: Heltec WiFi LoRa 32 (V2)
+*/
+
 #include <SPI.h>
 #include <LoRa.h>
 
-/* ================= CONFIG ================= */
-#define LORA_FREQ     915E6
-#define DEVICE_ID     0x01
+/* ================== PINOS HELTEC LORA32 V2 ================== */
+#define LORA_SCK   5
+#define LORA_MISO  19
+#define LORA_MOSI  27
+#define LORA_SS    18
+#define LORA_RST   14
+#define LORA_DIO0  26
 
-#define CMD_OFF       0
-#define CMD_ON        1
+#define LORA_BAND      915E6
+#define LORA_TX_POWER  15
 
-#define PIN_RELAY     27
-#define RX_TIMEOUT_MS 3000
-/* ========================================== */
+/* ================== APLICAÇÃO ================== */
+#define RELAY_PIN 13
 
-/* ================= CRC-8 ================= */
-uint8_t crc8(const uint8_t *data, uint8_t len) {
-  uint8_t crc = 0x00;
-  for (uint8_t i = 0; i < len; i++) {
-    crc ^= data[i];
-    for (uint8_t j = 0; j < 8; j++) {
-      if (crc & 0x80)
-        crc = (crc << 1) ^ 0x07;
-      else
-        crc <<= 1;
-    }
-  }
-  return crc;
-}
-/* ========================================= */
+#define STX 0xAA
+#define ETX 0x55
 
-struct CmdPacket {
-  uint8_t device_id;
+enum Cmd : uint8_t {
+  CMD_ON  = 0x01,
+  CMD_OFF = 0x02,
+  ACK_ON  = 0x81,
+  ACK_OFF = 0x82
+};
+
+typedef struct __attribute__((packed)) {
+  uint8_t stx;
   uint8_t cmd;
-  uint16_t seq;
-  uint8_t crc;
+  uint8_t seq;
+  uint8_t etx;
+} LoraPacket;
+
+enum RxState {
+  RX_IDLE,
+  RX_VALIDATE,
+  RX_ACTUATE,
+  RX_SEND_ACK
 };
 
-struct StatusPacket {
-  uint8_t device_id;
-  uint8_t estado_bomba;
-  uint8_t falha;
-  uint16_t seq;
-  uint8_t crc;
-};
+RxState rxState = RX_IDLE;
+LoraPacket rxPkt;
 
-/* ================= FSM RX ================= */
-typedef enum {
-  RX_INIT,
-  RX_WAIT_CMD,
-  RX_EXEC_CMD,
-  RX_SEND_STATUS
-} rx_state_t;
+/* ================== FUNÇÕES ================== */
 
-rx_state_t rx_state = RX_INIT;
+bool initLoRa() {
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
+  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
 
-/* ================= VAR ================= */
-CmdPacket last_cmd;
-uint16_t last_seq_rx = 0;
-unsigned long last_rx_time = 0;
-/* ======================================== */
+  if (!LoRa.begin(LORA_BAND)) {
+    Serial.println("[RX] Falha ao iniciar LoRa");
+    return false;
+  }
+
+  LoRa.setTxPower(LORA_TX_POWER);
+  LoRa.enableCrc();
+  LoRa.setSpreadingFactor(7);
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setCodingRate4(5);
+
+  Serial.println("[RX] LoRa inicializado");
+  return true;
+}
+
+void sendAck(uint8_t cmd, uint8_t seq) {
+  LoraPacket ack = {
+    STX,
+    (cmd == CMD_ON) ? ACK_ON : ACK_OFF,
+    seq,
+    ETX
+  };
+
+  LoRa.beginPacket();
+  LoRa.write((uint8_t*)&ack, sizeof(ack));
+  LoRa.endPacket();
+
+  Serial.println("[RX] ACK enviado");
+}
+
+/* ================== SETUP ================== */
 
 void setup() {
-  pinMode(PIN_RELAY, OUTPUT);
-  digitalWrite(PIN_RELAY, LOW);
+  Serial.begin(115200);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
 
-  LoRa.begin(LORA_FREQ);
+  while (!initLoRa());
 }
 
-void rx_fsm_run() {
-
-  switch (rx_state) {
-
-    case RX_INIT:
-      digitalWrite(PIN_RELAY, LOW);
-      rx_state = RX_WAIT_CMD;
-      break;
-
-    case RX_WAIT_CMD:
-      if (LoRa.parsePacket() == sizeof(CmdPacket)) {
-        CmdPacket pkt;
-        LoRa.readBytes((uint8_t*)&pkt, sizeof(pkt));
-
-        uint8_t crc_calc = crc8((uint8_t*)&pkt, sizeof(CmdPacket) - 1);
-
-        if (pkt.crc == crc_calc &&
-            pkt.device_id == DEVICE_ID &&
-            pkt.seq > last_seq_rx) {
-
-          last_seq_rx = pkt.seq;
-          last_cmd = pkt;
-          last_rx_time = millis();
-          rx_state = RX_EXEC_CMD;
-        }
-      }
-
-      if (millis() - last_rx_time > RX_TIMEOUT_MS)
-        digitalWrite(PIN_RELAY, LOW);
-      break;
-
-    case RX_EXEC_CMD:
-      digitalWrite(PIN_RELAY, (last_cmd.cmd == CMD_ON));
-      rx_state = RX_SEND_STATUS;
-      break;
-
-    case RX_SEND_STATUS: {
-      StatusPacket st;
-      st.device_id = DEVICE_ID;
-      st.estado_bomba = last_cmd.cmd;
-      st.falha = 0;
-      st.seq = last_cmd.seq;
-      st.crc = crc8((uint8_t*)&st, sizeof(StatusPacket) - 1);
-
-      LoRa.beginPacket();
-      LoRa.write((uint8_t*)&st, sizeof(st));
-      LoRa.endPacket();
-
-      rx_state = RX_WAIT_CMD;
-    } break;
-  }
-}
+/* ================== LOOP ================== */
 
 void loop() {
-  rx_fsm_run();
+
+  switch (rxState) {
+
+    case RX_IDLE:
+      if (LoRa.parsePacket()) {
+        if (LoRa.available() >= sizeof(rxPkt)) {
+          LoRa.readBytes((uint8_t*)&rxPkt, sizeof(rxPkt));
+          rxState = RX_VALIDATE;
+        }
+      }
+      break;
+
+    case RX_VALIDATE:
+      if (rxPkt.stx == STX && rxPkt.etx == ETX) {
+        rxState = RX_ACTUATE;
+      } else {
+        rxState = RX_IDLE;
+      }
+      break;
+
+    case RX_ACTUATE:
+      if (rxPkt.cmd == CMD_ON) {
+        digitalWrite(RELAY_PIN, HIGH);
+        Serial.println("[RX] Bomba LIGADA");
+      } 
+      else if (rxPkt.cmd == CMD_OFF) {
+        digitalWrite(RELAY_PIN, LOW);
+        Serial.println("[RX] Bomba DESLIGADA");
+      }
+      rxState = RX_SEND_ACK;
+      break;
+
+    case RX_SEND_ACK:
+      sendAck(rxPkt.cmd, rxPkt.seq);
+      rxState = RX_IDLE;
+      break;
+  }
 }
