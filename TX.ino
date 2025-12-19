4755215eae3d8ef6f10d2903bf6c793f41b45416
Miguel Ângelo <104993355+MiguelAngelo88@users.noreply.github.com>
2025-12-19 15:17:45 -0300

    Update TX.ino

--- TX.ino
+++ TX.ino
@@ -1,170 +1,146 @@
+/*
+  Controle de Bomba d'Água via LoRa
+  Módulo TX - Transmissor
+  MCU: Heltec WiFi LoRa 32 (V2)
+*/
+
 #include <SPI.h>
 #include <LoRa.h>
 
-/* ================= CONFIG ================= */
-#define LORA_FREQ      915E6
-#define DEVICE_ID      0x01
+/* ================== PINOS HELTEC LORA32 V2 ================== */
+#define LORA_SCK   5
+#define LORA_MISO  19
+#define LORA_MOSI  27
+#define LORA_SS    18
+#define LORA_RST   14
+#define LORA_DIO0  26
 
-#define CMD_OFF        0
-#define CMD_ON         1
+#define LORA_BAND      915E6
+#define LORA_TX_POWER  15
 
-#define PIN_KEY        12
-#define LED_GREEN      25
-#define LED_RED        26
+/* ================== APLICAÇÃO ================== */
+#define BUTTON_PIN   13
+#define ACK_TIMEOUT  1000  // ms
 
-#define TX_PERIOD_MS   200
-#define ACK_TIMEOUT_MS 800
-/* ========================================== */
+#define STX 0xAA
+#define ETX 0x55
 
-/* ================= CRC-8 ================= */
-uint8_t crc8(const uint8_t *data, uint8_t len) {
-  uint8_t crc = 0x00;
-  for (uint8_t i = 0; i < len; i++) {
-    crc ^= data[i];
-    for (uint8_t j = 0; j < 8; j++) {
-      if (crc & 0x80)
-        crc = (crc << 1) ^ 0x07;
-      else
-        crc <<= 1;
-    }
-  }
-  return crc;
-}
-/* ========================================= */
+enum Cmd : uint8_t {
+  CMD_ON  = 0x01,
+  CMD_OFF = 0x02,
+  ACK_ON  = 0x81,
+  ACK_OFF = 0x82
+};
 
-struct CmdPacket {
-  uint8_t device_id;
+typedef struct __attribute__((packed)) {
+  uint8_t stx;
   uint8_t cmd;
-  uint16_t seq;
-  uint8_t crc;
-};
+  uint8_t seq;
+  uint8_t etx;
+} LoraPacket;
 
-struct StatusPacket {
-  uint8_t device_id;
-  uint8_t estado_bomba;
-  uint8_t falha;
-  uint16_t seq;
-  uint8_t crc;
-};
-
-/* ================= FSM TX ================= */
-typedef enum {
-  TX_INIT,
-  TX_IDLE_OFF,
-  TX_IDLE_ON,
-  TX_SEND_CMD,
+enum TxState {
+  TX_IDLE,
+  TX_SEND,
   TX_WAIT_ACK,
-  TX_CONFIRMED,
-  TX_ERROR
-} tx_state_t;
+  TX_SUCCESS,
+  TX_TIMEOUT
+};
 
-tx_state_t tx_state = TX_INIT;
+TxState txState = TX_IDLE;
 
-/* ================= VAR ================= */
-uint8_t cmd_desejado = CMD_OFF;
-uint16_t seq_tx = 0;
+uint8_t seq = 0;
+unsigned long t0;
 
-unsigned long last_tx_time = 0;
-unsigned long ack_start_time = 0;
-/* ======================================== */
+/* ================== FUNÇÕES ================== */
+
+bool initLoRa() {
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
+  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+
+  if (!LoRa.begin(LORA_BAND)) {
+    Serial.println("[TX] Falha ao iniciar LoRa");
+    return false;
+  }
+
+  LoRa.setTxPower(LORA_TX_POWER);
+  LoRa.enableCrc();
+  LoRa.setSpreadingFactor(7);
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setCodingRate4(5);
+
+  Serial.println("[TX] LoRa inicializado");
+  return true;
+}
+
+void sendCommand(uint8_t cmd) {
+  LoraPacket pkt = { STX, cmd, seq, ETX };
+
+  LoRa.beginPacket();
+  LoRa.write((uint8_t*)&pkt, sizeof(pkt));
+  LoRa.endPacket();
+
+  Serial.println("[TX] Comando enviado");
+}
+
+bool receiveAck() {
+  LoraPacket pkt;
+  if (LoRa.available() < sizeof(pkt)) return false;
+
+  LoRa.readBytes((uint8_t*)&pkt, sizeof(pkt));
+
+  if (pkt.stx != STX || pkt.etx != ETX) return false;
+  if (pkt.seq != seq) return false;
+
+  if (pkt.cmd == ACK_ON || pkt.cmd == ACK_OFF) {
+    return true;
+  }
+
+  return false;
+}
+
+/* ================== SETUP ================== */
 
 void setup() {
-  pinMode(PIN_KEY, INPUT_PULLUP);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
+  Serial.begin(115200);
+  pinMode(BUTTON_PIN, INPUT);
 
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_RED, HIGH);
-
-  LoRa.begin(LORA_FREQ);
+  while (!initLoRa());
 }
 
-void tx_fsm_run() {
-
-  cmd_desejado = (digitalRead(PIN_KEY) == LOW) ? CMD_ON : CMD_OFF;
-
-  switch (tx_state) {
-
-    case TX_INIT:
-      tx_state = (cmd_desejado == CMD_ON) ? TX_IDLE_ON : TX_IDLE_OFF;
-      break;
-
-    case TX_IDLE_OFF:
-      digitalWrite(LED_GREEN, LOW);
-      digitalWrite(LED_RED, HIGH);
-
-      if (cmd_desejado == CMD_ON)
-        tx_state = TX_IDLE_ON;
-      else if (millis() - last_tx_time > TX_PERIOD_MS)
-        tx_state = TX_SEND_CMD;
-      break;
-
-    case TX_IDLE_ON:
-      digitalWrite(LED_GREEN, HIGH);
-      digitalWrite(LED_RED, LOW);
-
-      if (cmd_desejado == CMD_OFF)
-        tx_state = TX_IDLE_OFF;
-      else if (millis() - last_tx_time > TX_PERIOD_MS)
-        tx_state = TX_SEND_CMD;
-      break;
-
-    case TX_SEND_CMD: {
-      CmdPacket pkt;
-      pkt.device_id = DEVICE_ID;
-      pkt.cmd = cmd_desejado;
-      pkt.seq = ++seq_tx;
-      pkt.crc = crc8((uint8_t*)&pkt, sizeof(CmdPacket) - 1);
-
-      LoRa.beginPacket();
-      LoRa.write((uint8_t*)&pkt, sizeof(pkt));
-      LoRa.endPacket();
-
-      ack_start_time = millis();
-      last_tx_time = millis();
-      tx_state = TX_WAIT_ACK;
-    } break;
-
-    case TX_WAIT_ACK:
-      if (LoRa.parsePacket() == sizeof(StatusPacket)) {
-        StatusPacket st;
-        LoRa.readBytes((uint8_t*)&st, sizeof(st));
-
-        uint8_t crc_calc = crc8((uint8_t*)&st, sizeof(StatusPacket) - 1);
-
-        if (st.crc == crc_calc &&
-            st.device_id == DEVICE_ID &&
-            st.seq == seq_tx &&
-            st.falha == 0 &&
-            st.estado_bomba == cmd_desejado) {
-          tx_state = TX_CONFIRMED;
-        } else {
-          tx_state = TX_ERROR;
-        }
-      }
-
-      if (millis() - ack_start_time > ACK_TIMEOUT_MS)
-        tx_state = TX_ERROR;
-      break;
-
-    case TX_CONFIRMED:
-      if (millis() - last_tx_time > TX_PERIOD_MS)
-        tx_state = TX_SEND_CMD;
-
-      if (cmd_desejado == CMD_ON && digitalRead(PIN_KEY) != LOW)
-        tx_state = TX_IDLE_OFF;
-      if (cmd_desejado == CMD_OFF && digitalRead(PIN_KEY) == LOW)
-        tx_state = TX_IDLE_ON;
-      break;
-
-    case TX_ERROR:
-      digitalWrite(LED_GREEN, LOW);
-      digitalWrite(LED_RED, millis() % 300 < 150);
-      tx_state = TX_IDLE_OFF;
-      break;
-  }
-}
+/* ================== LOOP ================== */
 
 void loop() {
-  tx_fsm_run();
+
+  switch (txState) {
+
+    case TX_IDLE:
+      if (digitalRead(BUTTON_PIN) == HIGH) {
+        seq++;
+        sendCommand(CMD_ON);
+        t0 = millis();
+        txState = TX_WAIT_ACK;
+      }
+      break;
+
+    case TX_WAIT_ACK:
+      if (LoRa.parsePacket()) {
+        if (receiveAck()) {
+          txState = TX_SUCCESS;
+        }
+      } else if (millis() - t0 > ACK_TIMEOUT) {
+        txState = TX_TIMEOUT;
+      }
+      break;
+
+    case TX_SUCCESS:
+      Serial.println("[TX] ACK recebido. Bomba acionada.");
+      txState = TX_IDLE;
+      break;
+
+    case TX_TIMEOUT:
+      Serial.println("[TX] Timeout. Sem ACK.");
+      txState = TX_IDLE;
+      break;
+  }
 }
